4fcc56f0adbb848a3855d6cdad3da8254c84e84b
Rajin <rajshank>
2018-11-03 17:18:45 -0400

    Renamed and updated CameraTarget to CameraFocus

---
+++ CameraFocalPoint.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace UModules
+{
+    /// <summary>
+    /// A single focal point for a 2D camera
+    /// </summary>
+    public class CameraFocalPoint : ExtendedBehaviour
+    {
+        /// <summary>
+        /// CameraFocus component to affect (initializes to MainCamera if null)
+        /// </summary>
+        [Tooltip("Initialized to Main Camera if unset")]
+        [SerializeField]
+        private CameraFocus targetCameraFocus;
+
+        /// <summary>
+        /// Weight of focal point on creation
+        /// </summary>
+        [SerializeField]
+        private float initialWeight = 10;
+        /// <summary>
+        /// Speed of movement toward focal point on creation
+        /// </summary>
+        [SerializeField]
+        private float initialSpeed = 10;
+        /// <summary>
+        /// Max distance to affect camera on creation
+        /// </summary>
+        [SerializeField]
+        private float initialMaxDistance = 10;
+
+        /// <summary>
+        /// Initialize targetCameraFocus reference to MainCamera if null. 
+        /// Note: Must happen after CameraExtension Initialize (which sets MainCamera values)
+        /// </summary>
+        /// <seealso cref="CameraExtension.Initialize" />
+        public override void Initialize()
+        {
+            if (targetCameraFocus == null) targetCameraFocus = MainCamera.Focus;
+        }
+
+        /// <summary>
+        /// Add the focal point to the current set of active points when it is enabled
+        /// </summary>
+        private void OnEnable()
+        {
+            targetCameraFocus.AddTarget(transform, initialWeight, initialSpeed, initialMaxDistance);
+        }
+
+        /// <summary>
+        /// Remove the focal point from the current set of active points when it is disabled or destroyed
+        /// </summary>
+        private void OnDisable()
+        {
+            targetCameraFocus.RemoveTarget(transform);
+        }
+    }
+}
---
+++ CameraMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace UModules
+{
+    /// <summary>
+    /// Camera movement driver script that uses CameraFocus and CameraArea to control motion
+    /// Requires CameraFocus (any subclass) and CameraArea components on the same object
+    /// </summary>
+    [RequireComponent(typeof(CameraFocus))]
+    public class CameraMovement : ExtendedBehaviour
+    {
+        /// <summary>
+        /// Cached CameraFocus component, if it has been gotten
+        /// </summary>
+        private CameraFocus _CameraFocus;
+        /// <summary>
+        /// Get the CameraFocus component if it has been cached, return cached value otherwise
+        /// </summary>
+        public CameraFocus CameraFocus { get { return _CameraFocus ?? (_CameraFocus = GetComponent<CameraFocus>()); } }
+
+        // test
+        public float speed = 5;
+
+        // test
+        private void LateUpdate()
+        {
+            var targetInfo = CameraFocus.GetMovementTarget();
+            Vector2 movePosition = Vector2.Lerp(transform.position, targetInfo.position, speed * targetInfo.speed * Time.deltaTime);
+            transform.position = new Vector3(movePosition.x, movePosition.y, transform.position.z);
+        }
+    }
+}

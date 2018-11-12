2c23b7d47fbb8e9f63a62e459e42c1b5cbc84050
Rajin <rajshank>
2018-11-11 21:27:48 -0500

    Added independent pan and zoom speeds

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -26,12 +26,14 @@ namespace UModules
         /// </summary>
         /// <access>protected CameraExtension</access>
         protected CameraExtension extension;
+        
         /// <summary>
         /// The CameraFocus component to use (can be null).
         /// Always initialized to the component attached to the same object.
         /// </summary>
         /// <access>protected CameraFocus</access>
         protected CameraFocus focus;
+
         /// <summary>
         /// The CameraArea component to use (can be null).
         /// Always initialized to the component attached to the same object.
@@ -39,16 +41,17 @@ namespace UModules
         /// <access>protected CameraArea</access>
         protected CameraArea area;
 
-        /// <summary>
-        /// Base asymptotic speed for camera motion
-        /// </summary>
+        /// <summary>Base asymptotic speed for camera panning</summary>
         /// <access>protected float</access>
         [SerializeField]
-        protected float baseSpeed = 4;
+        protected float panSpeed = 4;
 
-        /// <summary>
-        /// Get CameraFocus and CameraArea components
-        /// </summary>
+        /// <summary>Base asymptotic speed for camera zooming</summary>
+        /// <access>protected float</access>
+        [SerializeField]
+        protected float zoomSpeed = 1;
+
+        /// <summary>Get CameraFocus and CameraArea components</summary>
         /// <access>public override void</access>
         public override void Initialize()
         {
@@ -57,14 +60,12 @@ namespace UModules
             area = GetComponent<CameraArea>();
         }
 
-        /// <summary>
-        /// Update the camera's properties through CameraExtension
-        /// </summary>
+        /// <summary>Update the camera's properties through CameraExtension</summary>
         /// <access>protected void</access>
         protected void LateUpdate()
         {
             Vector2 finalPosition = transform.position;
-            float finalSpeed = baseSpeed, finalZoom = float.NaN, finalPull = float.NaN;
+            float finalPanSpeed = panSpeed, finalZoomSpeed = zoomSpeed, finalZoom = float.NaN, finalPull = float.NaN;
 
             if (focus != null)
             {
@@ -72,7 +73,8 @@ namespace UModules
                 finalPosition = targetInfo.position;
                 finalZoom = targetInfo.zoom;
                 finalPull = targetInfo.pull;
-                finalSpeed *= targetInfo.speed;
+                finalPanSpeed *= targetInfo.speed;
+                finalZoomSpeed *= targetInfo.speed;
             }
             if (area != null)
             {
@@ -80,11 +82,11 @@ namespace UModules
                 finalPosition += areaOffset;
             }
 
-            Vector2 movePosition = Vector2.Lerp(extension.Pan, finalPosition, Time.deltaTime * finalSpeed);
+            Vector2 movePosition = Vector2.Lerp(extension.Pan, finalPosition, Time.deltaTime * finalPanSpeed);
             if (!(float.IsNaN(finalZoom) && float.IsNaN(finalPull)))
             {
-                extension.Zoom = Mathf.Lerp(extension.Zoom, finalZoom, Time.deltaTime * finalSpeed);
-                extension.Pull = Mathf.Lerp(extension.Pull, finalPull, Time.deltaTime * finalSpeed);
+                extension.Zoom = Mathf.Lerp(extension.Zoom, finalZoom, Time.deltaTime * finalZoomSpeed);
+                extension.Pull = Mathf.Lerp(extension.Pull, finalPull, Time.deltaTime * finalZoomSpeed);
             }
             extension.Pan = movePosition;
         }
